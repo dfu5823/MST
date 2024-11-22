58ae1f70529bc00a0cc9bc53f489aeb55e7f407c
Gustav Müller-Franzes <gustav.mueller-franzes@rwth-aachen.de>
2024-11-22 11:45:37 +0100

    update

--- mst/data/datasets/dataset_3d_lidc.py
+++ mst/data/datasets/dataset_3d_lidc.py
@@ -7,7 +7,8 @@ import torch
 from .augmentations.augmentations_3d import ImageOrSubjectToTensor, RescaleIntensity, ZNormalization, CropOrPad
 
 class LIDC_Dataset3D(data.Dataset):
-    PATH_ROOT = Path('/home/gustav/Coscine_Public/LIDC-IDRI/')
+    # PATH_ROOT = Path('/home/gustav/Coscine_Public/LIDC-IDRI/')
+    PATH_ROOT = Path('/home/gustav/Documents/datasets/LIDC-IDRI/')
     LABEL = 'Malignant'
 
     def __init__(
@@ -35,20 +36,18 @@ class LIDC_Dataset3D(data.Dataset):
                 tio.Resize(image_resize) if image_resize is not None else tio.Lambda(lambda x: x),
                 tio.Resample(resample) if resample is not None else tio.Lambda(lambda x: x),
                 tio.Lambda(lambda x: x.moveaxis(1, 2)), # Just for viewing, otherwise upside down
-                tio.RandomFlip((0,1,2)) if flip else tio.Lambda(lambda x: x),
-                CropOrPad(image_crop, random_center=random_center, mask_name='mask') if image_crop is not None else tio.Lambda(lambda x: x),
-                
-                tio.Clamp(-1024, 800),
-                RescaleIntensity((-1,1), in_min_max=(-1024, 800), per_channel=True),
+                CropOrPad(image_crop, random_center=random_center, mask_name='mask', padding_mode='minimum') if image_crop is not None else tio.Lambda(lambda x: x),
 
-                # tio.Clamp(-1000, 1000),
-                # RescaleIntensity((-1,1), in_min_max=(-1000, 1000), per_channel=True),
+                tio.Clamp(-1000, 1000),
+                RescaleIntensity((-1,1), in_min_max=(-1000, 1000), per_channel=True),
 
-                tio.RandomAffine(scales=0, degrees=(0, 0, 0, 0, 0,90), translation=0, isotropic=True) if random_rotate else tio.Lambda(lambda x: x),
-                tio.Lambda(lambda x: x+(torch.rand_like(x)-0.5)*0.25*torch.rand((1,),)[0], types_to_apply=tio.INTENSITY) if noise else tio.Lambda(lambda x: x),
-                # tio.Lambda(lambda x: (x-0.449)/0.226, types_to_apply=tio.INTENSITY),
-                # tio.Clamp((0-0.449)/0.226, (1-0.449)/0.226),
-                ImageOrSubjectToTensor() if to_tensor else tio.Lambda(lambda x: x) # [C, W, H, D] -> [C, D, H, W]
+                # tio.Lambda(lambda x: x.moveaxis(1, 2) if torch.rand((1,),)[0]<0.5 else x ) if random_rotate else tio.Lambda(lambda x: x), # WARNING: 1,2 if Subject, 2, 3 if tensor
+                tio.RandomAffine(scales=0, degrees=(0, 0, 0, 0, 0,90), translation=0, isotropic=True, default_pad_value='minimum') if random_rotate else tio.Lambda(lambda x: x),
+                tio.RandomFlip((0,1,2)) if flip else tio.Lambda(lambda x: x), # WARNING: Padding mask 
+                tio.Lambda(lambda x:-x if torch.rand((1,),)[0]<0.5 else x, types_to_apply=[tio.INTENSITY]) if noise else tio.Lambda(lambda x: x),
+                tio.RandomNoise(std=(0, 0.1)) if noise else tio.Lambda(lambda x: x),
+
+                ImageOrSubjectToTensor() if to_tensor else tio.Lambda(lambda x: x)             
             ])
         else:
             self.transform = transform
@@ -79,7 +78,7 @@ class LIDC_Dataset3D(data.Dataset):
         path_dir = self.path_root_data/rel_path
 
         filename = f'img_{nodule_idx}.nii.gz'
-        img = self.load_img(path_dir/filename)
+        img_org = self.load_img(path_dir/filename)
 
         filename = f'seg_{nodule_idx}.nii.gz'
         mask = self.load_map(path_dir/filename)
@@ -90,14 +89,16 @@ class LIDC_Dataset3D(data.Dataset):
                 masks[f'mask_{ann_idx}'] = self.load_map(path_dir/f"seg_{nodule_idx}_{ann_idx}.nii.gz" ) 
                     
         
-        subj = tio.Subject(img=img, mask=mask, **masks)
+        subj = tio.Subject(img=img_org, mask=mask, **masks)
         subj = self.transform(subj)
+
+        img = subj['img']
         
         if self.split == "test":
             masks = {key: subj[key] for key in masks.keys()}
 
-        return {'uid':uid, 'source': subj['img'], 'mask':subj['mask'], **masks, 'target':target, 
-                'affine':img.affine, 'path':str(rel_path), 'filename':filename}
+        return {'uid':uid, 'source': img, 'mask':subj['mask'], **masks, 'target':target, 
+                'affine':img_org.affine, 'path':str(rel_path), 'filename':filename}
     
 
     @classmethod
